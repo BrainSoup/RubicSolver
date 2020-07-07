ab874d112a4652fe759355551349b8621bd20017
BrainSoup <soulkeep1989@gmail.com>
2020-07-07 07:54:52 +0300

    Add basic scripts

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float CameraDistance = 10;
+    public float CameraSpeedH = 1;
+    public float CameraSpeedV = 1;
+    private bool _rotationMode = false;
+    private const int MouseMiddleButton = 2;
+
+
+    private Vector3 _prevPos = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        if (_rotationMode)
+        {
+            float delta_x = Input.mousePosition.x - _prevPos.x;
+            float delta_y = Input.mousePosition.y - _prevPos.y;
+            float h_angle = transform.rotation.eulerAngles.y;
+            float v_angle = transform.rotation.eulerAngles.x;
+            Vector3 newPos = Vector3.zero;
+            v_angle = v_angle > 180 ? v_angle - 360 : v_angle;
+            float new_v_angle = (90 - Mathf.Clamp((-delta_y * CameraSpeedV + v_angle), -45, 45));
+            float new_h_angle = (delta_x * CameraSpeedH + h_angle);
+
+            newPos.z = -CameraDistance * Mathf.Sin(new_v_angle * Mathf.Deg2Rad) * Mathf.Cos(new_h_angle * Mathf.Deg2Rad);
+            newPos.x = -CameraDistance * Mathf.Sin(new_v_angle * Mathf.Deg2Rad) * Mathf.Sin(new_h_angle * Mathf.Deg2Rad);
+            newPos.y = CameraDistance * Mathf.Cos(new_v_angle * Mathf.Deg2Rad);
+
+            transform.position = newPos;
+            _prevPos = Input.mousePosition;
+            transform.LookAt(Vector3.zero, Vector3.up);
+        }
+
+        if(Input.GetMouseButtonDown(MouseMiddleButton))
+        {
+            _rotationMode = true;
+            _prevPos = Input.mousePosition;
+        }
+        if(!Input.GetMouseButton(MouseMiddleButton))
+        {
+            _rotationMode = false;
+        }
+    }
+}
---
+++ Assets/Scripts/RubicBuilder.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RubicBuilder : MonoBehaviour
+{
+    public GameObject platePrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreateSide(Vector3.down, Quaternion.Euler(90, 0, 0), "down", new Color(0.8f, 0.65f, 0.1f));
+        CreateSide(Vector3.up, Quaternion.Euler(90, 0, 0), "up", Color.white);
+        CreateSide(Vector3.forward, Quaternion.Euler(0, 0, 0), "front", new Color(0.2f, 0.2f, 0.95f));
+        CreateSide(Vector3.back, Quaternion.Euler(0, 0, 0), "back", new Color(0.6f, 1, 0.65f));
+        CreateSide(Vector3.left, Quaternion.Euler(0, 90,0), "left", new Color(1, 0.6f, 0.65f));
+        CreateSide(Vector3.right, Quaternion.Euler(0, 90, 0), "right", new Color(1, 0.3f, 0.2f));
+    }
+
+    GameObject CreateSide(Vector3 v, Quaternion q, string name, Color color)
+    {
+        GameObject sideGO = new GameObject(name);
+        sideGO.transform.parent = transform;
+
+        for (int x = -1; x <= 1; x++)
+            for (int y = -1; y <= 1; y++)
+            {
+                var go = Instantiate(platePrefab, new Vector3(x, y, 0), Quaternion.identity, sideGO.transform);
+                var mesh = go.GetComponent<MeshRenderer>();
+                mesh.material.color = color;
+            }
+        sideGO.transform.rotation = q;
+        sideGO.transform.position = v * 1.5f;
+        return sideGO;
+    }
+
+
+}
